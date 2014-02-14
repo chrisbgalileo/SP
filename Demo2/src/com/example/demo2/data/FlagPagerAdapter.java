789cd252ed52acfa1638c3a6d862b59de9547ee7
chrisbgalileo <chitosabt@hotmail.com>
2014-02-13 23:01:16 -0600

    demos segunda semana

---
+++ Demo2/src/com/example/demo2/data/FlagPagerAdapter.java
@@ -0,0 +1,41 @@
+package com.example.demo2.data;
+
+import com.example.demo2.R;
+import com.example.demo2.fragments.FlagFragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class FlagPagerAdapter extends FragmentPagerAdapter {
+	private int[] arrayFlags = new int[]{
+			R.drawable.sakura,
+			R.drawable.sakuya,
+			R.drawable.momiji,
+			R.drawable.patch
+	};
+	
+	
+	public FlagPagerAdapter(FragmentManager fm) {
+		super(fm);
+		
+	}
+
+	@Override
+	public Fragment getItem(int position) {
+		
+		
+		Fragment fragment = new FlagFragment();
+		Bundle args = new Bundle();
+		args.putInt(FlagFragment.RESOURCE, arrayFlags[position]);
+		fragment.setArguments(args);
+		return null;
+	}
+
+	@Override
+	public int getCount() {
+		return arrayFlags.length;
+	}
+
+}
